8514d33312112811a5b20ce1a6903e9f07931c79
Michelle Loo <m_t_l_97@hotmail.com>
2020-12-15 15:11:50 -0500

    added UI/Play/Pause

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 //Import Styles 
 import './styles/app.scss'
 //Adding Components 
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from "./components/Library";
+//Import Util
+import data from './util';
 
 function App() {
+  //State
+ const [songs, setSongs] = useState(data()); 
+ const [currentSong, setCurrentSong] = useState(songs[0]);
+ const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong}
+              isPlaying={isPlaying}
+              setIsPlaying={setIsPlaying} />
+      <Library songs={songs}/>
     </div>
   );
 }
